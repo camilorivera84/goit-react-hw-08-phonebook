34c3b1cf7e6abc00236393471abf4e02a063283d
camilorivera84 <110140389+camilorivera84@users.noreply.github.com>
2023-10-05 12:20:01 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
